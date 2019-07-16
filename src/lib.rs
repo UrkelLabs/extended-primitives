cb0d5199d6a181073f3fd5fdd7589b261a638a06
kilpatty <seanpkilgarriff@gmail.com>
2019-07-16 12:58:09 -0500

    varint: inital

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod buffer;
 pub mod hash;
 pub mod uint256;
+pub mod varint;
 
 pub use buffer::{Buffer, BufferError};
 pub use hash::Hash;
 pub use uint256::Uint256;
+pub use varint::VarInt;
---
+++ src/varint.rs
@@ -0,0 +1,29 @@
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Debug)]
+pub struct VarInt(u64);
+
+impl VarInt {
+    pub fn encoded_size(&self) -> u64 {
+        match self.0 {
+            0...0xFC => 1,
+            0xFD...0xFFFF => 3,
+            0x10000...0xFFFFFFFF => 5,
+            _ => 9,
+        }
+    }
+
+    pub fn to_u64(self) -> u64 {
+        self.0
+    }
+}
+
+impl From<u32> for VarInt {
+    fn from(num: u32) -> Self {
+        VarInt(num as u64)
+    }
+}
+
+impl From<u64> for VarInt {
+    fn from(num: u64) -> Self {
+        VarInt(num)
+    }
+}
